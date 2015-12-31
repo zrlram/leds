92e9ff7abf0affab354a13c726c7d915ec7c4992
Raffael Marty <raffy@pixlcloud.com>
2015-12-31 01:13:10 -0800

    correct bottom half

--- my_sphere.py
+++ my_sphere.py
@@ -9,7 +9,7 @@ r = 1
 #row = [40, 40, 40, 32, 32, 32, 24, 24, 16, 8, 0] 	# height- rows
 #row = [40, 40, 32, 32, 24, 24, 24, 16, 16, 8, 0] 	# 512 - multiples of 8
 #row = [40, 36, 32, 32, 28, 24, 20, 16, 16, 12, 0] 	
-height = len(row) - 1 # this is only half of the sphere
+height = len(row)    # this is only half of the sphere
 lines = []
 # Upper sphere first 
 for up in range(0, height):
@@ -24,10 +24,24 @@ for up in range(0, height):
     #z = r*sin((phi)) - 0.07			# sphere height, not equally spaced
     lines.append('  {"point": [%.2f, %.2f, %.2f]}' %
 		(x, y, z))
-    #lines.append('  {"point": [%.2f, %.2f, %.2f]}' %
-		#(x, y, -z))
 
 # lower sphere
+# MINUS one row here! We don't want the bottom one
+for up in range(0, height-1):
+  z = (up+1) / float(height) - 0.07 		# equidistant, not on circumference
+  phi = asin(z) 
+  for over in range(0, row[up]):		# index starts at 0
+    theta = ((2 * pi) / row[up]) * over
+    # print phi, theta
+    x = r*cos((phi))*sin(theta)
+    y = r*cos((phi))*cos(theta)
+    lines.append('  {"point": [%.2f, %.2f, %.2f]}' %
+		(x, y, -z))
+
+print '[\n' + ',\n'.join(lines) + '\n]'
+
+exit()
+# lower sphere - OLD!!
 for up in range(0, height):
   phi = ((pi / 2) / (height+1)) * (up+1)
   for over in range(0, row[up]):
