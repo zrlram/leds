c386924bf4e576052ff7844e1e9c85171aea8f1c
Raffael Marty <raffy@pixlcloud.com>
2015-12-31 01:12:10 -0800

    correct geometry

--- connection.py
+++ connection.py
@@ -12,15 +12,21 @@ client = opc.Client('192.168.30.158:7890')
 # measured
 #row = [40, 38, 32, 28, 20, 14, 10, 0]  
 # 10' ball
-row = [50, 48, 40, 35, 25, 18, 13, 10, 8, 0]
-
+#row = [46, 46, 36, 31, 21, 16, 12, 12, 8, 0]
+# optimized 10'
+#row = [50, 50, 42, 35, 25, 18, 13, 10, 8, 0]
+# measured 10' - ideally
+#row = [46, 44, 44, 40, 36, 32, 28, 22, 16, 8]
+# measured 10' - tinkered
+row = [44, 44, 40, 36, 32, 28, 20, 12]
 
 # the one I have been using all the time:
 #row = [32, 32, 32, 32, 32, 32, 16, 16, 16, 8, 0]   # powers of 2
 height = len(row)-1        # this is only half of the sphere
                          # -1 because the height is minues one! LOL
 
-numLEDs = sum(row) * 2
+# bottom only has 7 rows
+numLEDs = sum(row) * 2 - 12
 print "num LEDs: ", numLEDs
 pixels = [ (0,0,0) ] * numLEDs
 # draw()
