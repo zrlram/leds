4ea2c69d197db2fab3026cc72752cbec5bb2ebbe
Raffael Marty <raffy@pixlcloud.com>
2016-01-13 07:19:16 -0800

    Moving color code into its own class

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 import json
 from connection import pixels, draw
-from math import floor
 import operator
 
 shaders = []            # list of shader functions to be called
@@ -50,35 +49,13 @@ def shader(p):
 def map_particles(particles, model):
     global _particles
     _particles = particles
-    register_shader(shader)
+    # do this in the init() of the show: register_shader(shader)
     map_pixels(model)
 
 def load_model(filename):
 
     with open(filename) as data_file:    
         data = json.load(data_file)
+    print "Loading model: %s" % filename
     return data
 
-def hsv(h,s,v,):
-    # convert HSV colors to RGB
-    # Normal hsv range is in [0,1], RGB is in [0,255]
-    # Hue values will wrap
-
-    h = (h % 1) *6
-    if (h < 0): h+=6
-    
-    i = int(floor(h))
-    f = h - i
-    p = v * (1 - s)
-    q = v * (1 - f * s)
-    t = v * (1 - (1 - f) * s)
-    r = int([v, q, p, p, t, v][i] * 255)
-    g = int([t, v, v, q, p, p][i] * 255)
-    b = int([p, p, t, v, v, q][i] * 255)
-
-    # cut to 0-255 range
-    r = max(0, min(r, 255))
-    g = max(0, min(g, 255))
-    b = max(0, min(b, 255))
-    
-    return (r, g, b)
