850e1e89f702a8284cdd928649c4ec9f84256175
Raffael Marty <raffy@pixlcloud.com>
2016-01-14 19:12:05 -0800

    fixing shader reset and clear

--- model.py
+++ model.py
@@ -4,23 +4,24 @@ import operator
 
 class Model(object):
 
-    shaders = []            # list of shader functions to be called
-    model = None
-
     def __init__(self, model_file):
+        self.model = None
         self.load_model(model_file)
+
+        self.shaders = []            # list of shader functions to be called
 
     def register_shader(self,shader):
         self.shaders.append(shader)
 
     def reset_shaders(self):
-        global shaders
-        shaders = []
+        self.shaders = []
 
     def map_pixels(self):
         # set all pixels by mapping each element of the "model" through 
         # "fn" and setting the corresponding pixel value. The "fn" function 
         # returns a tuple of three 8-bit RGB values.
+    
+        # print "active shaders: %s" % self.shaders
         
         for i, led in enumerate(self.model):
             # first one sets the base, others are 'add'-ed in
@@ -31,14 +32,23 @@ class Model(object):
 
         draw()
 
-    _particles = None
+    def clear(self):
+        black = (0,0,0)
 
-    def load_model(self,filename):
+        for i, led in enumerate(pixels):
+            pixels[i] = black
+       
+        # draw()        # -- this will be called implicitly
+
+    def load_model(self, filename):
 
         with open(filename) as data_file:    
             data = json.load(data_file)
         print "Loading model: %s" % filename
 
         self.model = data
+
+    def set_pixel(self, pixel, color):
+        pixels[pixel] = color
 
 
