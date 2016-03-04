9574d81c35acf112fbd9f40faad52b47aa90b8dd
Raffael Marty <raffy@pixlcloud.com>
2016-03-04 03:23:38 -0800

    adjusting height issues

--- utils.py
+++ utils.py
@@ -4,14 +4,14 @@ from math import pi
 def get_angles(position):
 
     negative = False
-    if position > (numLEDs / 2):
-        position = position - (numLEDs / 2)
+    if position >= sum(row):     # numLeds/2 is not half the sphere!
+        position = position - sum(row)
         negative = True
 
     up = 0
     over = 0
     remainder = 0
-    for go_up in range(0, height + 1):
+    for go_up in range(0, height):
         remainder += row[go_up]
         if remainder > position:
             up = go_up 
@@ -21,6 +21,8 @@ def get_angles(position):
     phi = ((pi/2) / height) * up
     theta = ( 2*pi / row[up] ) * over
     if negative:
-        phi = - phi
+        phi = - phi - ( pi/2 / height )
+
+    # print "phi, theta, up, over", phi, theta, up, over
 
     return (phi, theta)
