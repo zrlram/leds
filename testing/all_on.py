ef786f18df4f25f7499a799084527f55d1bb241f
Raffael Marty <raffy@pixlcloud.com>
2016-03-04 03:24:22 -0800

    no red one

--- testing/all_on.py
+++ testing/all_on.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python 
 
-# Open Pixel Control client: Every other light to solid white, others dark.
-
 import sys
 sys.path.append('..')
 sys.path.append('.')
 import opc
 
-numLEDs = 512
+numLEDs = 512       # just use all pins on all channels
 #client = opc.Client('192.168.30.158:7890')
 client = opc.Client('localhost:7890')
 
@@ -17,6 +15,7 @@ red = (255,0,0)
 on = [ (55, 55, 200) ] * numLEDs
 on[0] = red
 
-# make the first pixel red though!
+# reset first
 client.put_pixels(black)
+# make the first pixel red and set others to blue
 client.put_pixels(on)
