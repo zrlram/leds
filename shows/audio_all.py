e1df48df315282a41a4abc49e3475bb34e6f5733
Raffael Marty <raffy@pixlcloud.com>
2017-07-15 04:00:13 -0700

    clean up

--- shows/audio_all.py
+++ shows/audio_all.py
@@ -7,23 +7,19 @@ class AudioAll(looping_show.LoopingShow):
     ok_for_random = False
 
     name = "Audio Ball"
-    #max -10.9559531292 84.0577667883
-    #min -27.7098972444 40.331492511
 
     def __init__(self, geometry):
         looping_show.LoopingShow.__init__(self, geometry)
 
-        self.geometry = geometry
 	self.audio = audio.Audio()
-
-    def set_controls_model(self, cm):
-        self.cm = cm
+        self.duration = 1
 
     def update_at_progress(self, progress, new_loop, loop_instance):
 
-	(loud, pitch) = self.audio.audio_input()
-        color = col.hsv(pitch,pitch,loud)
-        #color = col.hsv(0.5,1,loud)
+	(loud, pitch, yy) = self.audio.audio_input()
+        #print int(loud * 100)  
+        #print "p",pitch
+        color = col.hsv(pitch,1.0,loud)
 
         # draw
         for i in range(self.geometry.get_nof_pixels()):
